914c723c5795bd147e7a33d0a582714cf3199339
steeltx <realsteel.gnu@gmail.com>
2024-01-30 00:09:03 -0600

    Se agrega heroicons

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,11 +1,16 @@
+import { CheckIcon } from '@heroicons/react/24/solid'
 import '../css/TodoItem.css';
 
 function TodoItem( { text, completed } ) {
     return (
     <li className="TodoItem">
-        <span className={`Icon Icon-check ${completed && "Icon-check--active"}`}>V</span>
+        <span className={`Icon Icon-check ${completed && "Icon-check--active"}`}>
+            <CheckIcon />
+        </span>
         <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}> { text } </p>
-        <span className="Icon Icon-delete">X</span>
+        <span className="Icon Icon-delete">
+            X
+        </span>
     </li>
     );
 }
