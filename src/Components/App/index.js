f8a97c472501131f3182e094449d2ec493e6d9b7
steeltx <realsteel.gnu@gmail.com>
2024-02-13 19:44:10 -0600

    Ejemplo de composición de componentes para compartir estado

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,13 +1,71 @@
-import { TodoProvider } from '../../TodoContext';
-import { AppUI } from './AppUI';
+import { useState } from 'react';
+// import { AppUI } from './AppUI';
 
-function App() {
+function App () {
 
-	return ( 
-		<TodoProvider>
-			<AppUI /> 
-		</TodoProvider>
+	const [state] = useState('estado compartido');
+
+	return (
+		<>
+			<TodoHeader>
+				<TodoCounter />
+				<TodoSearch />
+			</TodoHeader>
+			<TodoList>
+				<TodoItem state={state}/>
+			</TodoList>
+		</>
 	);
 }
+
+function TodoHeader ({children}) {
+	return (
+		<header>
+			{children}
+		</header>
+	);
+}
+
+function TodoList ({children}) {
+	return (
+		<>
+			{ children }
+		</>
+	);
+}
+
+function TodoCounter () {
+
+	return (
+		<>
+			<p>TodoCounter</p>
+		</>
+	);
+}
+
+function TodoSearch () {
+	return (
+		<>
+			<p>TodoSearch</p>
+		</>
+	);
+}
+
+function TodoItem ({state}) {
+	return (
+		<>
+			<p>TodoItem: {state}</p>
+		</>
+	);
+}
+
+// function App() {
+
+// 	return ( 
+// 		<TodoProvider>
+// 			<AppUI /> 
+// 		</TodoProvider>
+// 	);
+// }
 
 export default App;
